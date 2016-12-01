1046157fa2e062f12123e110c82851c2484216be
talha252 <talhacolakoglu@gmail.com>
2016-12-01 19:38:02 +0300

    Update gfycat plugin for python3 support

--- gallery_plugins/plugin_gfycat.py
+++ gallery_plugins/plugin_gfycat.py
@@ -7,13 +7,13 @@ except:
 def title(source):
     gfyId = re.findall(r'href=\".*gfycat.com/(\w+).*\">', source)[-1]
     link = 'https://gfycat.com/cajax/get/' + gfyId
-    respond = urllib.urlopen(link).read()
+    respond = urllib.urlopen(link).read().decode("utf8")
     username = re.findall(r'\"userName\":\"(.+?)\",' ,respond)[0]
     return username if username != "anonymous" else "gfycat " + gfyId
 
 def redirect(source):
     gfyId = re.findall(r'href=\".*gfycat.com/(\w+).*\">', source)[-1]
-    respond = urllib.urlopen('https://gfycat.com/cajax/get/' + gfyId).read()
+    respond = urllib.urlopen('https://gfycat.com/cajax/get/' + gfyId).read().decode("utf8")
     webmurl = re.findall(r'\"webmUrl\":\"(.+?)\",' ,respond)[0]
     # delete escape characters
     webmurl = webmurl.replace("\\","")
