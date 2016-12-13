275d84cd809162811690ef9b79352aea06aa0247
Jesse Almanrode <jesse@almanrode.com>
2016-12-13 09:31:27 -0700

    Added plugin for parsing 4Chan pages.

---
+++ gallery_plugins/plugin_4chan.py
@@ -0,0 +1,30 @@
+# Plugin for gallery_get.
+import re
+
+# Each definition can be one of the following:
+# - a string
+# - a regex string
+# - a function that takes source as a parameter and returns an array or a string.  (You may assume that re and urllib are already imported.)
+# If you comment out a parameter, it will use the default defined in __init__.py
+
+# identifier (default = name of this plugin after "plugin_") : If there's a match, we'll attempt to download images using this plugin.
+
+# title: parses the gallery page for a title.  This will be the folder name of the output gallery.
+title = r'<title>(.*?)</title>'
+
+# redirect: if the links in the gallery page go to an html instead of an image, use this to parse the gallery page.
+def redirect(source):
+    matcher = re.compile(r'File: <a href="(.*?\.4chan.org/.*?/[0-9]*\.jpg)"', re.I)
+    links = matcher.findall(source)
+    links = map(lambda x: 'http:' + x, links)
+    return links
+
+# direct_links: if redirect is non-empty, this parses each redirect page for a single image.  Otherwise, this parses the gallery page for all images.
+def direct_links(source):
+    matcher = re.compile(r'src=[\"\'](.+?\.jpe?g)[\"\']',re.I)
+    links = matcher.findall(source)
+    links = filter(lambda x: not "thumb" in x.lower(), links) # exclude thumbnails
+    return links
+
+# same_filename (default=False): if True, uses filename specified on remote link.  Otherwise, creates own filename with incremental index.
+same_filename = True
