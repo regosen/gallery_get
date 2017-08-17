8e00f14caced3da16a2cce4e7956bf97d49aa05f
Rego Sen <regosen@gmail.com>
2017-08-16 19:06:00 -0700

    move escaped dot outside of extension section in regex

--- gallery_plugins/plugin_imagefap.py
+++ gallery_plugins/plugin_imagefap.py
@@ -31,7 +31,7 @@ def redirect(source):
     return redirects
 
 # direct_links: if redirect is non-empty, this parses each redirect page for a single image.  Otherwise, this parses the gallery page for all images.
-direct_links = r'name="mainPhoto".+?(http://x.imagefapusercontent.com/.+?(\.jpe?g?|.png|.gif))'
+direct_links = r'name="mainPhoto".+?(http://x.imagefapusercontent.com/.+?\.(jpe?g?|png|gif))'
 
 # same_filename (default=False): if True, uses filename specified on remote link.  Otherwise, creates own filename with incremental index. 
 same_filename = True
