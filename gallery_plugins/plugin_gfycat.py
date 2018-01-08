4254d239d69ad66fb7edd8dd975e72c09222b3a6
Rego Sen <regosen@gmail.com>
2018-01-07 21:39:39 -0800

    update gfycat gallery plugin

--- gallery_plugins/plugin_gfycat.py
+++ gallery_plugins/plugin_gfycat.py
@@ -1,20 +1,19 @@
-import re
-from gallery_utils import *
+# Plugin for gallery_get.
 
-def title(source):
-    gfyId = re.findall(r'href=\".*gfycat.com/(\w+).*\">', source)[-1]
-    link = 'https://gfycat.com/cajax/get/' + gfyId
-    respond = urlopen_text(link)
-    username = re.findall(r'\"userName\":\"(.+?)\",' ,respond)[0]
-    return username if username != "anonymous" else "gfycat " + gfyId
+# Each definition can be one of the following:
+# - a string
+# - a regex string
+# - a function that takes source as a parameter and returns an array or a string.  (You may assume that re and urllib are already imported.)
+# If you comment out a parameter, it will use the default defined in __init__.py
 
-def redirect(source):
-    gfyId = re.findall(r'href=\".*gfycat.com/(\w+).*\">', source)[-1]
-    respond = urlopen_text('https://gfycat.com/cajax/get/' + gfyId)
-    webmurl = re.findall(r'\"webmUrl\":\"(.+?)\",' ,respond)[0]
-    # delete escape characters
-    webmurl = webmurl.replace("\\","")
-    # for some reason we can not connect via https
-    webmurl = webmurl.replace("https", "http")
-    return webmurl
-same_filename = True
+# identifier (default = name of this plugin after "plugin_") : If there's a match, we'll attempt to download images using this plugin.
+
+# title: parses the gallery page for a title.  This will be the folder name of the output gallery.
+title = r'itemProp=\"name\" content=\"(.+?)\"'
+
+# redirect: if the links in the gallery page go to an html instead of an image, use this to parse the gallery page.
+
+# direct_links: if redirect is non-empty, this parses each redirect page for a single image.  Otherwise, this parses the gallery page for all images.
+direct_links = r'src=\"(\S+?)\" type=\"video\/mp4\"'
+
+# same_filename (default=False): if True, uses filename specified on remote link.  Otherwise, creates own filename with incremental index. 
