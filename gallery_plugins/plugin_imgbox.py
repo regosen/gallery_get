cbe704f16577044d331e582362baddd420280842
Rego Sen <regosen@gmail.com>
2019-03-24 21:11:55 -0700

    fix issue #56

--- gallery_plugins/plugin_imgbox.py
+++ gallery_plugins/plugin_imgbox.py
@@ -14,6 +14,6 @@
 redirect = r'href=[\"\'](http://imgbox.com/.+?)[\"\']'
 
 # direct_links: if redirect is non-empty, this parses each redirect page for a single image.  Otherwise, this parses the gallery page for all images.
-direct_links = r'href="(https://i.imgbox.com/.+?)"'
+direct_links = r'href="(https://i\w+?.imgbox.com/.+?)"'
 
 # same_filename (default=False): if True, uses filename specified on remote link.  Otherwise, creates own filename with incremental index. 
