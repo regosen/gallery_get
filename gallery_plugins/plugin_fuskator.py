9de298b6ff51593d3a3de9c82ec9ecceed150823
Jesse Almanrode <jesse@almanrode.com>
2019-12-27 08:42:49 -0700

    Updated fuskator plugin to handle new thumbnail galleries
    Fixes Issue #60

--- gallery_plugins/plugin_fuskator.py
+++ gallery_plugins/plugin_fuskator.py
@@ -8,6 +8,7 @@ import sys
 # If you comment out a parameter, it will use the default defined in __init__.py
 
 # identifier (default = name of this plugin after "plugin_") : If there's a match, we'll attempt to download images using this plugin.
+identifier = 'fuskator.com'
 
 # title: parses the gallery page for a title.  This will be the folder name of the output gallery.
 def title(source):
@@ -17,20 +18,19 @@ def title(source):
 
 # redirect: if the links in the gallery page go to an html instead of an image, use this to parse the gallery page.
 def redirect(source):
-    matcher = re.compile(r'unescape\(\'(.*?)\'\)', re.I)
-    root = matcher.findall(source)[0].replace('%2f', '/')
-    root = 'http:' + root
-    matcher = re.compile(r'\'([0-9]*?\.jpe?g)\'', re.I)
-    images = matcher.findall(source)
-    links = set([root + x for x in images])
+    protocol = re.findall(r'link href=\"(http[s]?:\/\/)', source)[0]
+    # Are we working with a thumbnails page or the full size image page
+    thumbs_page = len(re.findall(r'<a class=\"navlink\" href=\"(.*?)\">View full images</a>', source))
+    if bool(thumbs_page):
+        links = re.findall(r'<div class=\'pic\'><a href=\'\/\/(.*?)\' rel=\'group\'>', source)
+    else:
+        links = re.findall(r'<img class=\'full\' .*? src=\'\/\/(.*?)\' ondragstart=.*?\/>', source)
+    links = map(lambda x: protocol + x, links)
+    links = set(links)  # Ensure we don't have any duplicates
     return links
 
 # direct_links: if redirect is non-empty, this parses each redirect page for a single image.  Otherwise, this parses the gallery page for all images.
-def direct_links(source):
-    matcher = re.compile(r'src=[\"\'](.+?\.jpe?g)[\"\']',re.I)
-    links = matcher.findall(source)
-    links = filter(lambda x: not "thumb" in x.lower(), links) # exclude thumbnails
-    return links
+direct_links = r'http[s]?:\/\/.*?\/large\/.*?\.jpg'
 
 # same_filename (default=False): if True, uses filename specified on remote link.  Otherwise, creates own filename with incremental index.
 same_filename = True
