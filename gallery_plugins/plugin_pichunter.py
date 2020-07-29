fa8236db16b1a1c2de079d7ecf09a645ceca35e8
RexQwerty <68962287+RexQwerty@users.noreply.github.com>
2020-07-29 13:23:59 -0400

    Adds support for site pichunter.com

---
+++ gallery_plugins/plugin_pichunter.py
@@ -0,0 +1,23 @@
+# Plugin for gallery_get.
+import re
+
+# Each definition can be one of the following:
+# - a string
+# - a regex string
+# - a function that takes source as a parameter and returns an array or a string.  (You may assume that re and urllib are already imported.)
+# If you comment out a parameter, it will use the default defined in __init__.py
+
+# identifier (default = name of this plugin after "plugin_") : If there's a match, we'll attempt to download images using this plugin.
+
+# title: parses the gallery page for a title.  This will be the folder name of the output gallery.
+title = r'<title>(.*?)</title>'
+
+# direct_links: if redirect is non-empty, this parses each redirect page for a single image.  Otherwise, this parses the gallery page for all images.
+def direct_links(source):
+    matcher = re.compile(r'href="(https://cdn.pichunter.com/[^"]*o.jpg)"',re.I)
+    links = matcher.findall(source)
+    links = filter(lambda x: not "thumb" in x.lower(), links) # exclude thumbnails
+    return links
+
+# same_filename (default=False): if True, uses filename specified on remote link.  Otherwise, creates own filename with incremental index.
+same_filename = True
