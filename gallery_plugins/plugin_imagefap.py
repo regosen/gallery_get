7365c6f5babd39e77ac17c56b342e149b5e19485
Rego Sen <regosen@gmail.com>
2020-08-24 10:54:18 -0700

    fix issue #76

--- gallery_plugins/plugin_imagefap.py
+++ gallery_plugins/plugin_imagefap.py
@@ -34,7 +34,7 @@ def redirect(source):
     return redirects
 
 # direct_links: if redirect is non-empty, this parses each redirect page for a single image.  Otherwise, this parses the gallery page for all images.
-direct_links = r'name=\"mainPhoto\".*?(https?://.*?\.imagefap.*?\.com/.*?\.(jpe?g?|png|gif).*?)\"'
+direct_links = r'name=\"mainPhoto\".*?(https?://.*?\.imagefap.*?\.com/.*?\.(jpe?g?|png|jfif|gif).*?)\"'
 
 # same_filename (default=False): if True, uses filename specified on remote link.  Otherwise, creates own filename with incremental index.
 same_filename = True
